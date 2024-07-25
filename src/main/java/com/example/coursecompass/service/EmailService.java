06cb5e575bf1cecb1106a9c2eb8c640f093abfed
Sabagan Chandrakanthan <sabagan22@icloud.com>
2024-07-25 13:56:54 -0400

    Email API with Spring Boot

---
+++ src/main/java/com/example/coursecompass/service/EmailService.java
@@ -0,0 +1,21 @@
+package com.example.coursecompass.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendContactEmail(String name, String email, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo("support@coursecompass.com");
+        mailMessage.setSubject("New Contact Form Submission");
+        mailMessage.setText("Name: " + name + "\nEmail: " + email + "\nMessage: " + message);
+        mailSender.send(mailMessage);
+    }
+}
