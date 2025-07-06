0cd82ea9c83b780c3d04b43774460c16bad0c321
Sabagan Chandrakanthan <sabagan22@icloud.com>
2025-07-06 18:48:20 -0400

    Using in memory db + ensure local webpage opens on jar double click

--- src/main/java/com/example/coursecompass/CourseCompassApplication.java
+++ src/main/java/com/example/coursecompass/CourseCompassApplication.java
@@ -4,12 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.awt.*;
+import java.net.URI;
+
 @SpringBootApplication
 @EnableWebMvc
 public class CourseCompassApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseCompassApplication.class, args);
+		try {
+			// Wait briefly for server to start
+			Thread.sleep(2000);
+			Desktop.getDesktop().browse(new URI("http://localhost:8080"));
+		} catch (Exception e) {
+			System.err.println("Failed to launch browser: " + e.getMessage());
+		}
 	}
 
 }
